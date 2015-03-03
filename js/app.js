09e1f75d6a93bc8334d4995412d99afd1786538e
Meaghan Bent <meaghan.bent@filemobile.com>
2015-03-03 13:43:19 -0500

    Bootstrap and Sass in, page routing and Foundation out.

--- js/app.js
+++ js/app.js
@@ -3,15 +3,7 @@
 angular.module('myApp', [
   //Angular Dependencies
   'ngRoute',
-  //Register page modules
-  'myApp.home',
-  'myApp.page',
   //Register component modules
   'myApp.topper',
   'myApp.foot'
-]).
-config(['$routeProvider', function($routeProvider) {
-  //Home is default
-  $routeProvider
-    .otherwise({ redirectTo: '/home' });
-}]);
+]);
