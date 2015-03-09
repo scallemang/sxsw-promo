b6a5155ac0023af3ecb4b2acbe8b22a34c23d062
Meaghan Bent <meaghan.bent@filemobile.com>
2015-03-08 22:55:49 -0400

    Fixing links for sub folder directory on staging

--- mvc/foot/foot-directive.js
+++ mvc/foot/foot-directive.js
@@ -1,54 +1,9 @@
 'use strict';
 angular.module('myApp.foot.foot-directive', [])
     .controller('FooterCtrl', ['$scope',function($scope) {
-    $scope.nav = {
-    0: {
-            "href" : "#/home",
-            "title" : "Homepage",
-            "text" : "Home",
-            "class" : ""
-        },
-        1: {
-            "href" : "#/page",
-            "title" : "Sample Page",
-            "text" : "Page ",
-            "class" : ""
-        },
-        2: {
-            "href" : "#",
-            "title" : "Sample Page",
-            "text" : "Page 2",
-            "class" : ""
-        },
-        3: {
-            "href" : "#",
-            "title" : "Sample Page",
-            "text" : "Page 3",
-            "class" : ""
-        },
-        4: {
-            "href" : "#",
-            "title" : "Sample Page",
-            "text" : "Page 4",
-            "class" : ""
-        },
-        5: {
-            "href" : "#",
-            "title" : "Sample Page",
-            "text" : "Page 5",
-            "class" : ""
-        },
-        6: {
-            "href" : "#",
-            "title" : "Sample Page",
-            "text" : "Page 6",
-            "class" : ""
-        }
-        
-        };
   }])
   .directive('foot', function() {
     return {
-      templateUrl: '/mvc/foot/foot.html'
+      templateUrl: 'mvc/foot/foot.html'
     };
   });
--- mvc/topper/topper-directive.js
+++ mvc/topper/topper-directive.js
@@ -1,42 +1,9 @@
 'use strict';
 angular.module('myApp.topper.topper-directive', [])
     .controller('TopperCtrl', ['$scope',function($scope) {
-    $scope.nav = {
-    0: {
-            "href" : "#/home",
-            "title" : "Homepage",
-            "text" : "Home",
-            "class" : ""
-        },
-        1: {
-            "href" : "#/method",
-            "title" : "Method Page",
-            "text" : "Method",
-            "class" : ""
-        },
-        2: {
-            "href" : "#/standard",
-            "title" : "Standard Page",
-            "text" : "Standard",
-            "class" : ""
-        },
-        3: {
-            "href" : "#/dna_mrkt",
-            "title" : "DNA Market",
-            "text" : "DNA Market",
-            "class" : ""
-        },
-        4: {
-            "href" : "#/about",
-            "title" : "About",
-            "text" : "About",
-            "class" : ""
-        }
-        
-        };
   }])
   .directive('topper', function() {
     return {
-      templateUrl: '/mvc/topper/topper.html'
+      templateUrl: 'mvc/topper/topper.html'
     };
   });
